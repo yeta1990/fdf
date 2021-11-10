88c0943057900d28058d2bb3259991b2669bfcae
albgarci <albgarci@student.42madrid.com>
2021-11-10 18:23:14 +0100

    drawing a square with 45º

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: albgarci <albgarci@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 11:22:44 by albgarci          #+#    #+#             */
-/*   Updated: 2021/11/05 17:13:32 by albgarci         ###   ########.fr       */
+/*   Updated: 2021/11/10 18:18:31 by albgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,18 @@
 //https://stackoverflow.com/c/42network/questions/173?rq=1
 //https://www.youtube.com/watch?v=2_BCYD_FwII
 //https://es.wikipedia.org/wiki/Algoritmo_de_Bresenham
+// https://studylib.net/doc/15067802/wire-frame-modeling-an-application-of-bresenham%E2%80%99s-line-dr...
+// https://k3no.medium.com/isometric-grids-in-python-40c0fad54552
+// https://www.davrous.com/2013/06/14/tutorial-part-2-learning-how-to-write-a-3d-soft-engine-from-scratch-in-c-ts-or-js-drawing-lines-triangles/
+//https://csustan.csustan.edu/~tom/Lecture-Notes/Graphics/Bresenham-Line/Bresenham-Line.pdf
+//https://www.geeksforgeeks.org/bresenhams-line-generation-algorithm/
 
 #include "mlx.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "fdf.h"
+
 //#include "mlx_int.h"
 
 typedef struct	s_params{
@@ -46,6 +53,79 @@ void checkleaks()
 	system("leaks fdf");
 }
 
+t_coords	create_coords(int x, int y, int z)
+{
+	t_coords	co;
+	
+	co.x = x;
+	co.y = y;
+	co.z = z;
+	return (co);
+}
+
+
+void	draw_line(int x0, int y0, int x1, int y1, t_params params)
+{
+	int	dx;
+	int	dy;
+	int	stepx;
+	int	stepy;
+
+	dx = x1 - x0;
+	dy = y1 - y0;
+	if (dy < 0)
+	{
+		dy = dy * -1;
+		stepy = -1;
+	}
+	else
+		stepy = 1;
+	if (dx < 0)
+	{
+		dx = dx * -1;
+		stepx = -1;
+	}
+	else
+		stepx = 1;
+	dy = dy * 2;
+	dx = dx * 2;
+	if ((0 <= x0) && (x0 < 600) && (0 <= y0) && (y0 < 600))
+		mlx_pixel_put(params.mlx, params.mlx_window, x0, y0, 16409700);
+	if (dx > dy)
+	{
+		int	fraction = dy - (dx / 2);
+		while (x0 != x1)
+		{
+			x0 += stepx;
+			if (fraction >= 0)
+			{
+				y0 += stepy;
+				fraction -= dx;
+			}
+			fraction += dy;
+			if ((0 <= x0) && (x0 < 600) && (0 <= y0) && (y0 < 600))
+				mlx_pixel_put(params.mlx, params.mlx_window, x0, y0, 16409700);
+		}
+	}
+	else
+	{
+		int	fraction = dx - (dy / 2);
+		while (y0 != y1)
+		{
+			if (fraction >= 0)
+			{
+				x0 += stepx;
+				fraction -= dy;
+			}
+			y0 += stepy;
+			fraction += dx;
+			if ((0 <= x0) && (x0 < 600) && (0 <= y0) && (y0 < 600))
+				mlx_pixel_put(params.mlx, params.mlx_window, x0, y0, 16409700);
+		}
+	}
+
+}
+
 int main(void)
 {
 	t_params	params;
@@ -54,12 +134,49 @@ int main(void)
 	params.mlx = mlx_init();
 	if (!params.mlx)
 		return (1);
-	params.mlx_window = mlx_new_window(params.mlx, 400, 400, "Pepe");
+	params.mlx_window = mlx_new_window(params.mlx, 1200, 1200, "Pepe");
 	if (!params.mlx_window)
 	{
 		write(1, "Error creating window\n", 22);
 	   	exit(1);
 	}
+
+	t_coords	*c;
+	int			z;
+	int			i;
+	int			j;
+
+	i = 0;
+	z = 0;
+	c = malloc(sizeof(t_coords) * 4);
+//	c[25] = 0;
+	while (i < 2)
+	{
+		j = 0;
+		while (j < 2)
+		{
+			c[z] = create_coords((i * 10 - j * 10) + 50, ((i*10 + j*10) / 2) + 50, 0);
+			j++;
+			z++;
+		}
+		i++;
+	}
+
+	draw_line(500, 500, 400, 550, params);
+	draw_line(500, 500, 600, 550, params);
+	draw_line(500, 600, 600, 550, params);
+	draw_line(500, 600, 400, 550, params);
+//	draw_line(600, 500, 600, 200, params);
+
+	z = 0;
+	while (z < 4)
+	{
+		printf("%d, %d\n", c[z].x, c[z].y);
+		z++;
+	}
+	
+
+/*	
 	int	i;
 	i = 0;
 	while (i < 50)
@@ -87,10 +204,10 @@ int main(void)
 	}
 
 	mlx_string_put(params.mlx, params.mlx_window, 10, 10, 16409700, "holaaaa");
-	
+*/	
 	//wait for "esc" key
 
-	mlx_hook(params.mlx_window, 6, (1L<<6), mouse_win3, 0);
+//	mlx_hook(params.mlx_window, 6, (1L<<6), mouse_win3, 0);
 	mlx_key_hook(params.mlx_window, draw_other, &params);
 	mlx_hook(params.mlx_window, 17, (1L<<17), byebye, &params);
 	mlx_loop(params.mlx);
